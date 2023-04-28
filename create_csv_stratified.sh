37b5c6ac853655b7fd66712bde59d909228b2288
Andrea Berti <andrea.berti11235@icloud.com>
2023-04-28 15:37:51 +0200

    Created script to generate the csv of the masses with patient label for stratification

---
+++ create_csv_stratified.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# print the title line in the csv file
+
+echo "file_name,patient_id,label" > training_bm.csv && \
+for y in {benign,cancer}; \
+do for z in $(ls ${y}); \
+do \
+name=$(echo ${z} | cut -d '_' -f1) && \
+echo ${y}/${z},${name}T,${y} >> training_bm.csv; \
+done; done
