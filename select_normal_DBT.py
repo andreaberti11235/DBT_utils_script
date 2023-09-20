17a96433f1d95a66e5f65b818a184e2e9116a52d
Andrea Berti <andrea.berti11235@icloud.com>
2023-09-20 09:15:20 +0200

    The script selects the first 100 normal DBT scans, renames them, and then moves them to the out folder

--- select_normal_DBT.py
+++ select_normal_DBT.py
@@ -22,49 +22,53 @@ def check_clean_create(dir):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Retrieve benign and cancer files and moves them in the destination folder.')
-    parser.add_argument('csv_box_file', help='csv file of bounding boxes (absolute path)')
+    parser.add_argument('csv_labels_file', help='csv file of labels (normal, actionable, benign, cancer) of DBT images (absolute path)')
     parser.add_argument('csv_paths_file', help='csv file of DBT paths (absolute path)')
     parser.add_argument('dest_dir', help='Destination directory of images (absolute path)')
     parser.add_argument('path_to_imgs', help='Absolute path to folder of input images (parent of Breast-Cancer-Screening-DBT)')
     args = parser.parse_args()
 
-    csv_box_file = args.csv_box_file
+    csv_labels_file = args.csv_labels_file
     csv_paths_file = args.csv_paths_file
     dest_dir = args.dest_dir
     path_to_imgs = args.path_to_imgs
 
     # Create subdir benign and cancer in dest_dir to store respective cases
-    benign_dest_dir = os.path.join(dest_dir, 'benign')
-    cancer_dest_dir = os.path.join(dest_dir, 'cancer')
+    normal_dest_dir = os.path.join(dest_dir, 'normal')
 
     # Check if benign_/cancer_dest_dir already exist. If so, create subdir old and move all previous contents there
-    check_clean_create(benign_dest_dir)
-    check_clean_create(cancer_dest_dir)
+    check_clean_create(normal_dest_dir)
 
-    df_box = pd.read_csv(csv_box_file)
+    df_labels = pd.read_csv(csv_labels_file)
     df_paths = pd.read_csv(csv_paths_file)
-    for idx, row in tqdm(df_box.iterrows(), total=len(df_box.index)):
+
+    count_normal = 0
+
+    for idx, row in tqdm(df_labels.iterrows(), total=len(df_labels.index)):
         # extract info from the csv
-        label = row['Class']
+        label_normal = row['Normal']
         patient_id = row['PatientID']
         study_id = row['StudyUID']
         view = row['View']
-        # take the path for that patient from the other csv (match of three conditions)
-        dcm_path = df_paths[(df_paths['PatientID'] == patient_id) & (df_paths['StudyUID'] == study_id) & (df_paths['View'] == view)]['descriptive_path'].iloc[0]
-        dcm_path = str(dcm_path)
-        # dcm_path = dcm_path.replace(" ", r"\ ")
-        dcm_path = dcm_path.replace('000000-', '000000-NA-')
+        if label_normal == 1:
+            # take the path for that patient from the other csv (match of three conditions)
+            dcm_path = df_paths[(df_paths['PatientID'] == patient_id) & (df_paths['StudyUID'] == study_id) & (df_paths['View'] == view)]['descriptive_path'].iloc[0]
+            dcm_path = str(dcm_path)
+            # dcm_path = dcm_path.replace(" ", r"\ ")
+            # names in the csv dont match the real ones, fixing the names
+            dcm_path = dcm_path.replace('000000-', '000000-NA-')
 
-        if label == 'benign':
-            out_path = benign_dest_dir
-        elif label == 'cancer':
-            out_path = cancer_dest_dir
+            # copy the patient's files in the correct folder. The name of the final file will be 
+            #  patientID_studyUID_view.dcm
+            src = os.path.join(path_to_imgs, dcm_path)
+            dst = os.path.join(normal_dest_dir, f'{patient_id}_{study_id}_{view}.dcm')
+            shutil.copy(src=src, dst=dst)
+            count_normal += 1
+
+            if count_normal == 1000:
+                break
+        
         else:
-            raise Exception(f'Wrong label ({label}) identified in csv file.')
-
-        # copy the patient's files in the correct folder. The name of the final file will be 
-        #  patientID_studyUID_view.dcm
-        src = os.path.join(path_to_imgs, dcm_path)
-        dst = os.path.join(out_path, f'{patient_id}_{study_id}_{view}.dcm')
-        shutil.copy(src=src, dst=dst)
+            pass
+        
 
