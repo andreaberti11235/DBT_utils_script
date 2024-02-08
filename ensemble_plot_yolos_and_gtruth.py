0595e9cca3d8c0b0343c7e1666e70bc119b447c9
Andrea Berti <andrea.berti11235@icloud.com>
2024-02-08 16:29:22 +0100

    found the problem, when building new_image, I was pasting ensemble_pil instead of ensemble_rgb

--- ensemble_plot_yolos_and_gtruth.py
+++ ensemble_plot_yolos_and_gtruth.py
@@ -57,7 +57,6 @@ def draw_box(
 def main():
     parser = argparse.ArgumentParser(description='Plot the resuts of yolov5 and yolov8, together with the ground truth.')
     parser.add_argument('ensemble_dir', help='Absolute path of the folder containing the labels resulting from the ensemble inference')
-    parser.add_argument('img_val_dir', help='Absolute path of the folder containing the validation images')
     parser.add_argument('yolov5_dir', help='Absolute path of the folder containing the images resulting from yolov5 inference')
     parser.add_argument('yolov8_dir', help='Absolute path of the folder containing the images resulting from yolov8 inference')
     parser.add_argument('gt_dir', help='Absolute path to folder of input images (parent of images/ and labels/)')
@@ -65,7 +64,6 @@ def main():
     args = parser.parse_args()
 
     ensemble_dir = args.ensemble_dir
-    img_val_dir = args.img_val_dir
     yolov5_dir = args.yolov5_dir
     yolov8_dir = args.yolov8_dir
     gt_dir = args.gt_dir
@@ -91,8 +89,7 @@ def main():
         label = os.path.join(path_to_labels, f'{name}.txt')
         img_pil = Image.open(img)
         img_npy = np.array(img_pil)
-        img_ensemble_pil = Image.open(os.path.join(img_val_dir, f'{name}.png'))
-        ensemble_npy = np.array(img_ensemble_pil)
+        ensemble_npy = np.copy(img_npy)
         img_width = img_npy.shape[1]
         img_height = img_npy.shape[0]
 
@@ -136,24 +133,23 @@ def main():
         # Create an ImageDraw object
         draw_ensemble = ImageDraw.Draw(ensemble_rgb)
         # Draw the text on the image at a given position and color
-        draw_ensemble.text((500, 100), "Yolo ensemble", font=font, fill=fill)
+        draw_ensemble.text((500, 100), "Yolo ensemble", font=font, fill=fill, image=ensemble_rgb)
         draw_ensemble.text((500, 250), f"{name}", font=font2, fill=fill)
 
-        # # add the text with the conf value for each detection
-        # if os.stat(ensemble_label).st_size != 0:
-        #     for idx in ensemble_df.index:
-        #         x_center = ensemble_df.iloc[idx][1]
-        #         y_center = ensemble_df.iloc[idx][2]
-        #         width = ensemble_df.iloc[idx][3]
-        #         height = ensemble_df.iloc[idx][4]
-        #         confidence = ensemble_df.iloc[idx][5]
+        # add the text with the conf value for each detection
+        if os.stat(ensemble_label).st_size != 0:
+            for idx in ensemble_df.index:
+                x_center = ensemble_df.iloc[idx][1]
+                y_center = ensemble_df.iloc[idx][2]
+                width = ensemble_df.iloc[idx][3]
+                height = ensemble_df.iloc[idx][4]
+                confidence = ensemble_df.iloc[idx][5]
 
-        #         x_position = x_center - width/2
-        #         y_position = y_center - height/2
+                x_position = x_center - width/2
+                y_position = y_center - height/2
 
-        #         # non sto stampando le conf, perché?
-        #         # draw_ensemble.text((x_position, y_position - offset), f"{np.round(confidence, decimals=2)}", font=font2, fill=fill)
-        #         draw_ensemble.text((x_position, y_position - offset), f"CIAO", font=font3, fill=fill)
+                # non sto stampando le conf, perché?
+                draw_ensemble.text((x_position, y_position - offset), f"{np.round(confidence, decimals=2)}", font=font2, fill=fill)
 
         # draw_ensemble.text((600, 400), f"CIAOCIAO", font=font3, fill=fill)
 
@@ -176,29 +172,29 @@ def main():
 
 
         df = pd.read_csv(label, sep=' ', header=None)
-        # for idx in df.index:
-        #     # scorro su tutte le bbox e prendo i vari valori
-        #     x_center = df.iloc[idx][1]
-        #     y_center = df.iloc[idx][2]
-        #     width = df.iloc[idx][3]
-        #     height = df.iloc[idx][4]
+        for idx in df.index:
+            # scorro su tutte le bbox e prendo i vari valori
+            x_center = df.iloc[idx][1]
+            y_center = df.iloc[idx][2]
+            width = df.iloc[idx][3]
+            height = df.iloc[idx][4]
 
-        #     # modifico l'immagine 'disegnandoci' dentro le bbox
-        #     img_npy = draw_box(img_npy, x_center=x_center, y_center=y_center, 
-        #              width=width, height=height, 
-        #              img_width=img_width, img_height=img_height, color=255)
+            # modifico l'immagine 'disegnandoci' dentro le bbox
+            img_npy = draw_box(img_npy, x_center=x_center, y_center=y_center, 
+                     width=width, height=height, 
+                     img_width=img_width, img_height=img_height, color=255)
         
         img_gt_pil = Image.fromarray(img_npy)
         img_gt_rgb = img_gt_pil.convert('RGB')
 
         # Create an ImageDraw object
-        # draw_gt = ImageDraw.Draw(img_gt_rgb)
-        # # Draw the text on the image at a given position and color
-        # draw_gt.text((500, 100), "Ground truth", font=font, fill=fill)
+        draw_gt = ImageDraw.Draw(img_gt_rgb)
+        # Draw the text on the image at a given position and color
+        draw_gt.text((500, 100), "Ground truth", font=font, fill=fill)
 
         # Get the width and height of each image
         width_gt, height_gt = img_gt_rgb.size
-        width_ensemble, height_ensemble = ensemble_pil.size
+        width_ensemble, height_ensemble = ensemble_rgb.size
         width_v5, height_v5 = yolov5_pil.size
         width_v8, height_v8 = yolov8_pil.size
 
@@ -211,7 +207,7 @@ def main():
         new_image.paste(yolov5_pil, (0, 0))
         new_image.paste(img_gt_rgb, (width_v5, 0))
         new_image.paste(yolov8_pil, (width_v5 + width_gt, 0))
-        new_image.paste(ensemble_pil, (width_v5 + width_gt + width_v8, 0))
+        new_image.paste(ensemble_rgb, (width_v5 + width_gt + width_v8, 0))
 
         # salvataggio immagini
         out_name = os.path.join(out_dir, f'{name}.png')
